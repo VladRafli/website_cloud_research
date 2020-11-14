0e2dbf28b8bf1252e4a3ff0be62bea4de5921343
VladRafli <rafli.jaskandi@gmail.com>
2020-11-14 11:33:36 +0700

    Finishing client front-end

--- new-client/src/Components/Footer.js
+++ new-client/src/Components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <footer>
+        <footer className="main-footer">
             <h3>Created with ❤ by FRYF Research Team</h3>
             <p>&#169;2020, FRYF Research Team</p>
         </footer>
--- new-client/src/Components/MainMenu.js
+++ new-client/src/Components/MainMenu.js
@@ -6,11 +6,186 @@ export default function MainMenu() {
     <main className="main-menu">
       <header className="banner">
         <div className="banner-filter">
-          <h3>Sunib Blog</h3>
+          <h1>Sunib Blog</h1>
           <p>Knowledge sharing and Fun blogging!</p>
         </div>
       </header>
-      <main className="content"></main>
+      <section className="content">
+        <section className="featured-post">
+          <header className="featured-post-header">
+            <h1>Featured Post</h1>
+          </header>
+          <article className="featured-post-content">
+            <header className="featured-post-title">
+              <h1>Featured Title!</h1>
+            </header>
+            <section className="featured-post-body">
+              <figure>
+                <img
+                  src="https://images.pexels.com/photos/4451721/pexels-photo-4451721.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+                  alt="Wall Street"
+                />
+                <figcaption>Wall Street</figcaption>
+              </figure>
+              <div>
+                <p>
+                  <span>L</span>orem ipsum dolor sit amet consectetur
+                  adipisicing elit. Eum laudantium vitae atque laboriosam
+                  deserunt, explicabo nobis odit blanditiis. Error commodi
+                  aliquid, dolorum blanditiis dolorem, molestias beatae
+                  reprehenderit aperiam eius nemo repellat repudiandae itaque
+                  recusandae consectetur et perspiciatis. Possimus minus at
+                  nobis sapiente, autem nisi quibusdam est vel saepe qui
+                  eligendi.
+                </p>
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut
+                  delectus ipsa aspernatur magni soluta repellendus facere sit
+                  aliquid recusandae ipsam cum velit, facilis sed reprehenderit
+                  molestias in? Necessitatibus totam sequi itaque velit porro
+                  commodi voluptatem blanditiis quibusdam optio pariatur ipsa
+                  vero nulla laudantium quam veniam atque ipsum, aut laborum
+                  omnis.
+                </p>
+                <a href="#">Read more...</a>
+              </div>
+            </section>
+            <footer className="featured-post-footer">
+              <pre>Date: 14-11-2020</pre>
+              <pre>By: Admin</pre>
+            </footer>
+          </article>
+        </section>
+        <section className="normal-post">
+          <header className="normal-post-header">
+            <h1>New Posts</h1>
+          </header>
+          <article className="normal-post-content">
+            <header className="normal-post-title">
+              <h1>Post Title!</h1>
+            </header>
+            <section className="normal-post-body normal-post-1">
+              <figure>
+                <img
+                  src="https://images.pexels.com/photos/4451721/pexels-photo-4451721.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+                  alt="Wall Street"
+                />
+                <figcaption>Wall Street</figcaption>
+              </figure>
+              <div>
+                <p>
+                  <span>L</span>orem ipsum dolor sit amet consectetur
+                  adipisicing elit. Eum laudantium vitae atque laboriosam
+                  deserunt, explicabo nobis odit blanditiis. Error commodi
+                  aliquid, dolorum blanditiis dolorem, molestias beatae
+                  reprehenderit aperiam eius nemo repellat repudiandae itaque
+                  recusandae consectetur et perspiciatis. Possimus minus at
+                  nobis sapiente, autem nisi quibusdam est vel saepe qui
+                  eligendi.
+                </p>
+                <a href="#">Read more...</a>
+              </div>
+            </section>
+            <footer className="normal-post-footer">
+              <pre>Date: 14-11-2020</pre>
+              <pre>By: Admin</pre>
+            </footer>
+          </article>
+          <article className="normal-post-content normal-post-2">
+            <header className="normal-post-title">
+              <h1>Post Title!</h1>
+            </header>
+            <section className="normal-post-body">
+              <figure>
+                <img
+                  src="https://images.pexels.com/photos/4451721/pexels-photo-4451721.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+                  alt="Wall Street"
+                />
+                <figcaption>Wall Street</figcaption>
+              </figure>
+              <div>
+                <p>
+                  <span>L</span>orem ipsum dolor sit amet consectetur
+                  adipisicing elit. Eum laudantium vitae atque laboriosam
+                  deserunt, explicabo nobis odit blanditiis. Error commodi
+                  aliquid, dolorum blanditiis dolorem, molestias beatae
+                  reprehenderit aperiam eius nemo repellat repudiandae itaque
+                  recusandae consectetur et perspiciatis. Possimus minus at
+                  nobis sapiente, autem nisi quibusdam est vel saepe qui
+                  eligendi.
+                </p>
+                <a href="#">Read more...</a>
+              </div>
+            </section>
+            <footer className="normal-post-footer">
+              <pre>Date: 14-11-2020</pre>
+              <pre>By: Admin</pre>
+            </footer>
+          </article>
+          <article className="normal-post-content normal-post-3">
+            <header className="normal-post-title">
+              <h1>Post Title!</h1>
+            </header>
+            <section className="normal-post-body">
+              <figure>
+                <img
+                  src="https://images.pexels.com/photos/4451721/pexels-photo-4451721.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+                  alt="Wall Street"
+                />
+                <figcaption>Wall Street</figcaption>
+              </figure>
+              <div>
+                <p>
+                  <span>L</span>orem ipsum dolor sit amet consectetur
+                  adipisicing elit. Eum laudantium vitae atque laboriosam
+                  deserunt, explicabo nobis odit blanditiis. Error commodi
+                  aliquid, dolorum blanditiis dolorem, molestias beatae
+                  reprehenderit aperiam eius nemo repellat repudiandae itaque
+                  recusandae consectetur et perspiciatis. Possimus minus at
+                  nobis sapiente, autem nisi quibusdam est vel saepe qui
+                  eligendi.
+                </p>
+                <a href="#">Read more...</a>
+              </div>
+            </section>
+            <footer className="normal-post-footer">
+              <pre>Date: 14-11-2020</pre>
+              <pre>By: Admin</pre>
+            </footer>
+          </article>
+          <article className="normal-post-content normal-post-4">
+            <header className="normal-post-title">
+              <h1>Post Title!</h1>
+            </header>
+            <section className="normal-post-body">
+              <figure>
+                <img
+                  src="https://images.pexels.com/photos/4451721/pexels-photo-4451721.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+                  alt="Wall Street"
+                />
+                <figcaption>Wall Street</figcaption>
+              </figure>
+              <div>
+                <p>
+                  <span>L</span>orem ipsum dolor sit amet consectetur
+                  adipisicing elit. Eum laudantium vitae atque laboriosam
+                  deserunt, explicabo nobis odit blanditiis. Error commodi
+                  aliquid, dolorum blanditiis dolorem, molestias beatae
+                  reprehenderit aperiam eius nemo repellat repudiandae itaque
+                  recusandae consectetur et perspiciatis. Possimus minus at
+                  nobis sapiente, autem nisi quibusdam est vel saepe qui
+                  eligendi.
+                </p>
+                <a href="#">Read more...</a>
+              </div>
+            </section>
+            <footer className="normal-post-footer">
+              <pre>Date: 14-11-2020</pre>
+              <pre>By: Admin</pre>
+            </footer>
+          </article>
+        </section>
+      </section>
     </main>
   );
 }
