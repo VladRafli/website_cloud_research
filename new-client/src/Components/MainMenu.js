44767ca9c2228dcee717111e24692bd88a65b6a5
VladRafli <rafli.jaskandi@gmail.com>
2020-11-17 20:48:59 +0700

    MainMenu on Client fixed

--- new-client/src/Components/MainMenu.js
+++ new-client/src/Components/MainMenu.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 export default class MainMenu extends React.Component {
   state = {
-    posts: []
+    posts: [],
   };
 
   componentDidMount() {
@@ -15,7 +15,7 @@ export default class MainMenu extends React.Component {
     axios
       .get("http://localhost:5000/get")
       .then((res) => {
-        const data = res.data;
+        const data = res.data.result;
         this.setState({ posts: data });
         console.log("Data has been recieved!");
         console.log(data);
@@ -26,77 +26,69 @@ export default class MainMenu extends React.Component {
   }
 
   displayFeaturedBlogPost(posts) {
-    let spanRes = posts.post_body;
-    // spanRes.substring(0, 1)
-    let bodyRes = posts.post_body;
-    // bodyRes.substring(1, posts.post_body.length)
-    let datePosted = posts.post_date_posted;
-    // datePosted.toDateString()
     if (!posts.length) return null;
 
     return posts.map((post, index) => {
-      return (
-        <article className="featured-post-content" key={index}>
-          <header className="featured-post-title">
-            <h1>Featured Title!</h1>
-          </header>
-          <section className="featured-post-body">
-            <figure>
-              <img src={decodeURIComponent(post.post_image)} alt="Image Alt" />
-              <figcaption>Image Caption</figcaption>
-            </figure>
-            <div>
-              <p>
-                <span>{spanRes}</span>
-                {bodyRes}
-              </p>
-              <a href="#">Read more...</a>
-            </div>
-          </section>
-          <footer className="featured-post-footer">
-            <pre>Date: {datePosted}</pre>
-            <pre>By: {post.post_author}</pre>
-          </footer>
-        </article>
-      );
+      if (index === 0) {
+        return (
+          <article className="featured-post-content" key={index}>
+            <header className="featured-post-title">
+              <h1>{post.post_title}</h1>
+            </header>
+            <section className="featured-post-body">
+              <figure>
+                <img
+                  src={decodeURIComponent(post.post_image)}
+                  alt="Image Alt"
+                />
+                <figcaption>Image Caption</figcaption>
+              </figure>
+              <div>
+                <p>{post.post_body}</p>
+                <a href="#">Read more...</a>
+              </div>
+            </section>
+            <footer className="featured-post-footer">
+              <pre>Date: {post.post_date_posted}</pre>
+              <pre>By: {post.post_author}</pre>
+            </footer>
+          </article>
+        );
+      }
     });
   }
 
   displayNormalBlogPost(posts) {
     let c = "normal-post-body normal-post-";
-    let spanRes = posts.post_body;
-    // spanRes.substring(0, 1)
-    let bodyRes = posts.post_body;
-    // bodyRes.substring(1, posts.post_body.length)
-    let datePosted = posts.post_date_posted;
-    // datePosted.toDateString()
     if (!posts.length) return null;
 
     return posts.map((post, index) => {
-      return (
-        <article className="normal-post-content" key={index}>
-          <header className="normal-post-title">
-            <h1>Post Title!</h1>
-          </header>
-          <section className={c.concat(index + 1)}>
-            <figure>
-              <img src={decodeURIComponent(post.post_image)} alt="Image Alt" />
-              <figcaption>Image Caption</figcaption>
-            </figure>
-            <div>
-              <p>
-                <span>{spanRes}</span>
-                {bodyRes}
-              </p>
-              <a href="#">Read more...</a>
-            </div>
-          </section>
-          <footer className="normal-post-footer">
-            <pre>Date: {datePosted}</pre>
-            <pre>By: {post.post_author}</pre>
-          </footer>
-        </article>
-      );
+      if (index !== 0) {
+        return (
+          <article className="normal-post-content" key={index}>
+            <header className="normal-post-title">
+              <h1>{post.post_title}</h1>
+            </header>
+            <section className={c.concat(index + 1)}>
+              <figure>
+                <img
+                  src={decodeURIComponent(post.post_image)}
+                  alt="Image Alt"
+                />
+                <figcaption>Image Caption</figcaption>
+              </figure>
+              <div>
+                <p>{post.post_body}</p>
+                <a href="#">Read more...</a>
+              </div>
+            </section>
+            <footer className="normal-post-footer">
+              <pre>Date: {post.post_date_posted}</pre>
+              <pre>By: {post.post_author}</pre>
+            </footer>
+          </article>
+        );
+      }
     });
   }
 
