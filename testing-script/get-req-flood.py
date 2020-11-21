3651ea0ca0be9932e55a4ce2e73bbf22339b6565
VladRafli <rafli.jaskandi@gmail.com>
2020-11-21 18:22:07 +0700

    Added testing script

---
+++ testing-script/get-req-flood.py
@@ -0,0 +1,18 @@
+# Get Request Flood
+# Effective for some second
+# Because NGINX will block this request after some second
+# When you try to reload the client page and inspect the network waterfall
+# You'll notice load time increase when this script is running
+# Inspired by saphyra.py script by Anonymous
+# Use at own risk!
+# VladRafli
+
+import requests
+
+i = 0
+while i < 100:
+    x = requests.get('http://localhost:80')
+    if x.status_code != 200:
+        print("Get request failed with code", x.status_code)
+    if i == 100:
+        print("Command successfully executed")
